2ae607d8e0e1eb57690a7f8713482643e1fd1981
Jesse <jseapayne@gmail.com>
2015-08-19 12:57:01 +0100

    Indentation added
    Slightly more readable now

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
-function addToList (item) {
-$('ol#items').append("<li>" + item + "</li>" )
-};
+  function addToList (item) {
+   $('ol#items').append("<li>" + item + "</li>" )
+  };
 $(document).on('click','#add-to-list', function() {
   $('ol#items').append("<li>"+$('input').val()+"</li>");
-});
-addToList("build a website");
+  });
+  
+  addToList("build a website");
 });
